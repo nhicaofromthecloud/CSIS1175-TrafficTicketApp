27ba6fc95fefac01270d1f4df0cc913b1db124f7
nhicaofromthecloud <114954151+nhicaofromthecloud@users.noreply.github.com>
2023-05-21 15:02:19 -0700

    Add files via upload

---
+++ NhiCao_TicketApp/NhiCao_TicketApp/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace NhiCao_TicketApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char moreTicket = 'Y'; //a character to keep track of if user says 'y' for yes to more tickets or 'n' for no more tickets
+
+            while (moreTicket == 'Y')
+            {
+
+                GetTicketDetails(out string stId, out char stCat,
+                            out int spLimit, out int spReported);
+                Ticket myTicket = new Ticket(stId, stCat, spLimit, spReported);
+                WriteLine(myTicket); //same as WriteLine(myTicket.ToString())
+
+                //get user input to see if user enters 'Y' for more tickets or 'N' for no more tickets
+                Write("Do you want to process another ticket? (Y - yes, N- no): ");
+                while (!char.TryParse(ReadLine(), out moreTicket) || (moreTicket != 'Y' && moreTicket != 'N'))
+                {
+                    Write("Please re-enter your choice. Do you want to process another ticket? (Y-yes, N-no): ");
+                }
+            }
+
+            if (moreTicket == 'N')
+            {
+                WriteLine("Thank you for paying your ticket. Goodbye.");
+            }
+
+
+        }
+
+        static void GetTicketDetails(out string studentId,
+                                out char studentCat,
+                                out int speedLimit,
+                                out int speedReported)
+        {
+            studentId = "";
+            studentCat = 'U'; //U refers to undefined, just as an initial value, all intial values not really needed here
+            speedLimit = 0;
+            speedReported = 0;
+            WriteLine("Let us get the ticket details for the ticket...."); 
+            
+            Write("Please enter student id: ");
+            studentId = ReadLine();
+            while (studentId=="")
+            {
+                Write("Student Id cannot be empty. Please re-enter: ");
+                studentId = ReadLine();
+            }
+
+            Write("Please enter student category (1-freshman, 2-sophomore, 3-junior, 4-senior): "); //NOT operator is !=
+            while(!char.TryParse(ReadLine(), out studentCat) || studentCat!='1' && studentCat != '2' && studentCat != '3' && studentCat != '4')
+            {
+                //enters loop only if value is other than 1, 2, 3 or 4
+                Write("Please Re-enter the student category (1-freshman, 2-sophomore, 3-junior, 4-senior): ");
+            }
+
+            Write("Please enter speed limit: ");
+            while(!int.TryParse(ReadLine(), out speedLimit) || speedLimit <= 0)
+            {
+                WriteLine("Invalid speed - should be positive number."); 
+                Write("Please re-enter speed limit: ");
+            }
+
+            Write("Please enter speed reported: ");
+            while(!int.TryParse(ReadLine(), out speedReported) || speedReported < speedLimit )
+            {
+                WriteLine("Invalid speed - should be positive number greater than speed limit");
+                Write("Please re-enter speed reported: ");
+            }
+        }
+    }
+}
---
+++ NhiCao_TicketApp/NhiCao_TicketApp/Ticket.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhiCao_TicketApp
+{
+    internal class Ticket
+    {
+        //auto-properties
+        public string StudentId { get; set; }
+        public char StudentCategory { get; set; }
+       
+        public int SpeedLimit {get; set; }
+        public int SpeedReported { get; set; }
+
+
+        //computed read-only property
+        //does not need to return a value outside of get
+        public string CategoryName
+        {
+            get { 
+                
+                if (StudentCategory=='1')
+                {
+                    return "Freshman";
+                }
+                else if (StudentCategory =='2')
+                {
+                    return "Sophomore";
+                }
+
+                else if (StudentCategory == '3')
+                {
+                    return "Junior";
+
+                }
+                else if (StudentCategory == '4')
+                {
+                    return "Senior";
+                }
+                else 
+                {
+                    return "Error";
+                }
+
+            }
+            
+        }
+
+        //Constructors have same name with class name
+        //there's no "out" bc it isnt a method.
+        //OUT is used to create loaddetails()
+        //to load users' inputs into the object
+        public Ticket (string studentId, char studentCategory, 
+             int speedLimit, int speedReported)
+        {
+            StudentId = studentId;
+            StudentCategory = studentCategory;
+            SpeedLimit = speedLimit;
+            SpeedReported = speedReported;
+
+        }
+
+        public decimal Fine
+        {
+            get
+            {
+                int speedDiff = SpeedReported - SpeedLimit; //local variable inside get method
+                if (speedDiff > 0)
+                {
+                    decimal fine = 75.0m + (speedDiff / 5) * 87.5m;
+                    if (StudentCategory == '4' && speedDiff > 20)
+                    {
+                        fine += 200; //same as fine = fine + 200
+                    }
+                    else if (StudentCategory == '4' && speedDiff <= 20)
+                    {
+                        fine += 50;
+                    }
+                    else if (StudentCategory == '1' && speedDiff < 20)
+                    {
+                        fine -= 50; //same as fine = fine - 50
+                    }
+                    else if (speedDiff >= 20)
+                    {
+                        fine += 100;
+                    }
+                    return fine; //puts value of fine in Fine property
+                }
+                else
+                {
+                    return 0; //Fine is set to 0
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string outputStr = "\n Student ID: " + StudentId
+                + "\n Student Category: " + StudentCategory
+                + "\n " + CategoryName
+                + "\n Speed Limit: " + SpeedLimit
+                + "\n Speed Reported: " + SpeedReported
+                + "\n Fine: " + Fine.ToString("C");
+            return outputStr;
+        }
+    }
+}
